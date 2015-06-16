bca28712e83567fb77da009087b0703354da2c33
youxinyu <1015701481@qq.com>
2015-06-16 22:44:28 +0800

    font

--- js/hc.js
+++ js/hc.js
@@ -22,6 +22,7 @@ $('.nav-toggle').on('click', function () {
     }
 });
 */
+    $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
 $(function() {
     $('.project-box').click(function() {
