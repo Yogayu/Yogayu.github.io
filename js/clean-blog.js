52f4b674c6074ea60bd5ebfa3f0749fd4cf21fee
youxinyu <1015701481@qq.com>
2015-08-22 21:51:40 +0800

    new type

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -7,6 +7,28 @@
 // Contact Form Scripts
 
 $(function() {
+//在文章中查找title并填充到div AnchorContent中
+$(".post-content").find("h2,h3,h4,h5,h6").each(function(i,item){
+    var tag = $(item).get(0).localName;
+    $(item).attr("id","wow"+i);
+    $("#AnchorContent").append('<li><a class="new'+tag+' anchor-link" onclick="return false;" href="#" link="#wow'+i+'">'+(i+1)+" · "+$(this).text()+'</a></li>');
+    $(".newh2").css("margin-left",0);
+    $(".newh3").css("margin-left",20);
+    $(".newh4").css("margin-left",40);
+    $(".newh5").css("margin-left",60);
+    $(".newh6").css("margin-left",80);
+});
+$("#AnchorContentToggle").click(function(){
+    var text = $(this).html();
+    if(text=="目录[-]"){
+        $(this).html("目录[+]");
+        $(this).attr({"title":"展开"});
+    }else{
+        $(this).html("目录[-]");
+        $(this).attr({"title":"收起"});
+    }
+    $("#AnchorContent").toggle();
+});
 
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
